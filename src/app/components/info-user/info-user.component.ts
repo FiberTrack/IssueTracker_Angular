dce93169d8edf6557137d54bdcc938dc7e362418
AbelBatalla <abelbatallaf@gmail.com>
2023-05-26 13:51:53 +0200

    dades obtingudes

--- src/app/components/info-user/info-user.component.ts
+++ src/app/components/info-user/info-user.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-info-user',
@@ -6,5 +9,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./info-user.component.css']
 })
 export class InfoUserComponent {
+
+//data
+  data: any = {};
+  prova = "klk"
+  usuariId = 0
+
+  constructor(private apiService: ApiService, private route: ActivatedRoute, private location: Location) { }
+
+  ngOnInit(): void {
+    //GET USER DATA
+    this.usuariId = this.route.snapshot.params['usuari_id'];
+    console.log(this.usuariId); // Use the parameter as required
+
+    this.getUserById(); 
+
+  }
+
+  getUserById(): void {
+    const userId = this.route.snapshot.params['usuari_id'];
+    if (userId) {
+      this.apiService.getUserById(parseInt(userId)).subscribe(
+        user => {
+          this.data = user;
+          console.log(this.data);
+        },  
+        error => {
+          console.log('Error al obtenir Usuari: ', error);
+        }
+      );
+    }
+
+  }
+/*
+  getIssueById(): void {
+    const issueId = this.route.snapshot.paramMap.get('id');
+    if (issueId) {
+      this.apiService.getIssueById(parseInt(issueId)).subscribe(
+        issue => {
+          this.data = issue;
+          console.log(this.data);
+        },
+        error => {
+          console.log('Error al obtener la issue:', error);
+        }
+      );
+    }
+  }
+
+
+
+
+
+  getUsuaris(){
+    this.apiService.getUsuaris().subscribe(usuaris => {
+      this.usuaris = usuaris;
+  
+      // Verificar si todos los usuarios tienen la propiedad 'id'
+      const usuariosConID = this.usuaris.every(user => user.hasOwnProperty('id'));
+      if (!usuariosConID) {
+        console.error('Algunos usuarios no tienen la propiedad "id".');
+        return;
+      }
+  
+      this.usuaris.sort((a, b) => a.id - b.id); // Ordenar los usuarios por su ID
+      console.log(this.usuaris);
+    });
+  }
+  */
+
+
 
 }
